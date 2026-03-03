142f0e4f94ba3f93134d00cbf4a036d5616018df
Wentworth666 <2961169300@qq.com>
2026-03-03 21:06:39 +0800

    添加 Person 类练习 - 面向对象基础（类、对象、方法）

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    public class Person {
+        String name;   // 属性
+        int age;
+
+        void eat() {   // 方法
+            System.out.println(name + " 正在吃饭...");
+        }
+
+        public static void main(String[] args) {
+            Person zhangsan = new Person();  // 创建对象
+            zhangsan.name = "张三";
+            zhangsan.age = 20;
+            zhangsan.eat();  // 调用方法
+        }
+    }
+
+}
